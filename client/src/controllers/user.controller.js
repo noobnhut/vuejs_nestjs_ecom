b663679792ee32aaf798ee03b79393ad42a57727
Minh Nhựt Nguyễn <nguyen.nhut.99.2017@gmail.com>
2024-05-06 13:35:13 +0700

    client_register_update

--- client/src/controllers/user.controller.js
+++ client/src/controllers/user.controller.js
@@ -8,20 +8,41 @@ export default {
   // Lấy danh sách các user
   getUsers() {
     try {
-        return axios.get(`${API_URL}/users`);
-      } catch (error) {
-        console.error('Lỗi lấy list user', error);
-        throw error; 
-      }
+      return axios.get(`${API_URL}/users`);
+    } catch (error) {
+      console.error('Lỗi lấy list user', error);
+      throw error;
+    }
   },
 
   // Lấy thông tin của một user theo id
   getUser(userId) {
     try {
-        return axios.get(`${API_URL}/users/${userId}`);
-      } catch (error) {
-        console.error('Lỗi lấy list user theo id', error);
-        throw error; 
-      }
-  },  
+      return axios.get(`${API_URL}/users/${userId}`);
+    } catch (error) {
+      console.error('Lỗi lấy list user theo id', error);
+      throw error;
+    }
+  },
+
+  // hàm đăng ký user
+  register( formData) {
+    try {
+      const fullname = formData.get("fullname");
+      const address = formData.get("address");
+      const email = formData.get("email");
+      const numberphone = formData.get("numberphone");
+      const password = formData.get("password");
+      return axios.post(`${API_URL}/users`,
+        {
+          email:email,
+          password:password,
+          address:address,
+          fullname:fullname,
+          numberphone:numberphone,
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 };
